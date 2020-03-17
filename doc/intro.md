466b41a4e3c6f1fe2549b17be845781ecb1684b7
Milan Ray <raytmilan@gmail.com>
2020-03-17 16:58:37 -0700

    Initial Project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to adhyaya
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/adhyaya/core_test.clj
@@ -0,0 +1,7 @@
+(ns adhyaya.core-test
+  (:require [clojure.test :refer :all]
+            [adhyaya.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
